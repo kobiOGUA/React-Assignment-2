799e59f561c5d9f9c2a41234c4264356ae8ec7a4
Kobi Oguadinma <kobioguadinma@gmail.com>
2025-10-06 22:47:50 +0100

    Little update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ function Projects() {
   return (
     <section>
       <h1>This is the projects section</h1>
-      <p>I ahve many projects like A guessing game, portfolio website, etc</p>
+      <p>I have many projects like A guessing game, portfolio website, etc</p>
     </section>
   );
 }
